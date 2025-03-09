3d77c7b009d8a658dfd562d3f23293b0245f61b2
Faisal Hasan <me@faisal.one>
2025-03-09 23:48:04 +0600

    logo missing fixed

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,8 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <meta name="description" content="{{ $settings['description'] }}">
-    <meta name="keywords" content="{{ $settings['keywords'] }}">
+    <meta name="description" content="{{ $settings['description'] ?? '' }}">
+    <meta name="keywords" content="{{ $settings['keywords'] ?? '' }}">
     <meta property="og:image" content="{{ $settings['og'] ?? asset('assets/images/og-image.png') }}">
     
 	<title>
